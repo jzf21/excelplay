8fe06a33e8aeaefd43b077ab02c0189d4745f888
jzf21 <jozefneelamkavil2003@gmail.com>
2024-12-31 15:02:30 +0530

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import dynamic from 'next/dynamic'
 import { GameCard } from '@/components/game-card'
 import { LoginButton } from '@/components/login-button'
-import localFont from 'next/font/local';
+
+import logo from './assets/play2022.png'
+import Image from 'next/image'
 // import Loader from '@/components/loader';
 // Import the Background component dynamically to avoid SSR issues
 const Background = dynamic(() => import('@/components/background'), { ssr: false })
-  const starJedi = localFont({
-  src: "./fonts/Starjedi.ttf",
-  variable: "--font-star-jedi",
-  weight: "400",
-});
+//   const starJedi = localFont({
+//   src: "./fonts/Starjedi.ttf",
+//   variable: "--font-star-jedi",
+//   weight: "400",
+// });
 export default function Home() {
 
   const games = [
@@ -42,11 +44,12 @@ export default function Home() {
       <Background />
       <div className="relative z-10">
         <header className="flex justify-between items-center p-6">
-          <h1 className={`text-4xl font-bold text-white ${starJedi.variable} italic`}>EXCEL PLAY</h1>
+          <Image src={logo} alt="logo" width={100} height={100} />
           <LoginButton />
         </header>
         <div className="container   h-[80%]  mx-auto px-4 py-12 ">
-          <h1 className="text-7xl font-bold text-white text-center md:my-8 my-4  --font-star-jedi tracking-wide ">EXCEL PLAY</h1>
+         {/* <Image src={logo} alt="logo" width={200} height={200} /> */}
+          <h1 className="text-6xl font-bold text-white text-center my-6">play now</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto ">
             {games.map((game) => (
               <GameCard key={game.title} {...game} />
